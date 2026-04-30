8267878e22e73660de91f2b19fea36cbe9439dd1
ObyMoods <aobi5198@gmail.com>
2026-04-30 22:44:52 +0700

    Upload config/index.js

---
+++ config/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // Telegram API credentials
+    apiId: "23308353",
+    apiHash: "a0e3e35baf28eff517d44f31a3fce61f",
+
+    // Bot credentials
+    botToken: "8165120036:AAE8tcHLbKbLGiK6Jvn0GloLlFsQiCAMbkA",
+    botUsername: "rajauserbot_bot", // GAUSAH PAKE @
+
+    // Bot settings
+    prefix: ".",
+    
+    // Isi No Dana Lu
+    dana: "083876317793",
+    
+    // Admin 
+    adminUser: "crit_v", // GAUSAH PAKE @
+
+    // Owner ID
+    ownerId: "8272883006",
+
+    // Paths
+    sessionsDir: "data/sessions",
+};
