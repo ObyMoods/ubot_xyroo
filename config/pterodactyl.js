57282ffe3d88731eb1fd079f808c235da960abea
ObyMoods <aobi5198@gmail.com>
2026-04-30 22:44:54 +0700

    Upload config/pterodactyl.js

---
+++ config/pterodactyl.js
@@ -0,0 +1,11 @@
+/**
+ * Pterodactyl Panel Settings
+ */
+
+module.exports = {
+    domain: "https://panel.example.com",
+    adminApiKey: "YOUR_ADMIN_API_KEY",
+    clientApiKey: "YOUR_CLIENT_API_KEY",
+    defaultEggs: 15,
+    defaultLocation: 1
+};
